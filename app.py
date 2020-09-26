9b76a676f53d2145a68fa303a5fd69956357e840
Shiva Hanchate <shiva.hanchate@capita.com>
2020-09-26 18:09:34 +0100

    add class to the code

--- app.py
+++ app.py
@@ -7,78 +7,74 @@ app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 
-def get_Board_Cards(api_key, token, boardId):
-    endpoint = f"https://api.trello.com/1/boards/{boardId}/cards?"
-    response = requests.get(endpoint, params={"key": api_key, "token": token}).json()
-    return response
+class Board:
+
+    def get_Board_Cards(api_key, token, boardId):
+        endpoint = f"https://api.trello.com/1/boards/{boardId}/cards?"
+        response = requests.get(endpoint, params={"key": api_key, "token": token}).json()
+        return response
 
 
-def add_Card(api_key, token, newToDoItem):
-    endpoint = f"https://api.trello.com/1/cards?name={newToDoItem}"
-    response = requests.post(endpoint, params={"key": api_key, "token": token, "idList": toDoListId}).json()
-    return response
+class List:
+
+    def get_List_Cards(api_key, token, CardId ):
+        endpoint = f"https://api.trello.com/1/cards/{CardId}/list?"
+        response = requests.get(endpoint, params={"key": api_key, "token": token}).json()
+        return response
 
 
-def move_Card_NotStarted_InProgress(id):
-    endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={doingListId}"
-    response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
-    return response
+class Card:
 
+    def add_Card(api_key, token, newToDoItem):
+        endpoint = f"https://api.trello.com/1/cards?name={newToDoItem}"
+        response = requests.post(endpoint, params={"key": api_key, "token": token, "idList": toDoListId}).json()
+        return response
 
-def move_Card_InProgress_Done(id):
-    endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={doneListId}"
-    response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
-    return response
+    def move_Card_NotStarted_InProgress(id):
+        endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={doingListId}"
+        response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
+        return response
 
+    def move_Card_InProgress_Done(id):
+        endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={doneListId}"
+        response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
+        return response
 
-def move_Card_Done_Todo(id):
-    endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={toDoListId}"
-    response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
-    return response
+    def move_Card_Done_Todo(id):
+        endpoint = f"https://api.trello.com/1/cards/{id}/idList?value={toDoListId}"
+        response = requests.put(endpoint, params={"key": api_key, "token": token}).json()
+        return response
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         newToDoItem = request.form["NewToDoItem"]
-        add_Card(api_key, token, newToDoItem)
-        data = get_Board_Cards(api_key, token, boardId)
+        Card.add_Card(api_key, token, newToDoItem)
+        data = Board.get_Board_Cards(api_key, token, boardId)
         return render_template('index.html', items=data)
-    data = get_Board_Cards(api_key, token, boardId)
+    data = Board.get_Board_Cards(api_key, token, boardId)
     print(data)
     return render_template('index.html', items = data)
 
 
 @app.route('/items/<id>/inprogress')
 def move_item_to_inprogress(id):
-    move_Card_NotStarted_InProgress(id)
+    Card.move_Card_NotStarted_InProgress(id)
     return redirect(url_for('index'))
 
 
 @app.route('/items/<id>/done')
 def move_item_to_done(id):
-    move_Card_InProgress_Done(id)
+    Card.move_Card_InProgress_Done(id)
     return redirect(url_for('index'))
 
 
 @app.route('/items/<id>/todo')
 def move_item_todo(id):
-    move_Card_Done_Todo(id)
+    Card.move_Card_Done_Todo(id)
     return redirect(url_for('index'))
 
-
-"""
-@app.route('/', methods=["POST"])
-def additem():
-    NewToDoItem = request.form["NewToDoItem"]
-    session.add_item(NewToDoItem)
-    return render_template('index.html', items=session.get_items())
-
-@app.route('/items/<id>/complete')
-def complete_item(id):
-    session.complete_item(id)
-    return redirect(url_for('index'))
-"""
 
 if __name__ == '__main__':
     app.run()
