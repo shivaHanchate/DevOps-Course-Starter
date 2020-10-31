d1987059ff73979819a97c373a1b7dcf391688f4
Shiva Hanchate <shiva.hanchate@capita.com>
2020-10-31 13:05:08 +0000

    Part2 Step2,3,4 refactored trello_api and added integration test to test index page.

--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,11 @@
 import pytest
 from dotenv import find_dotenv, load_dotenv
+from unittest.mock import patch, Mock
+import os
 
-import trello_api
-from app import app, create_app
+from requests.models import Response
+
+from app import create_app
 
 @pytest.fixture
 def client():
@@ -10,11 +13,46 @@ def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
 # Create the new app.
-    test_app = app.create_app()
+    test_app = create_app()
 # Use the app to create a test_client that can be used in our tests.
     with test_app.test_client() as client:
         yield client
 
-def test_index_page(mock_get_requests, client):
-    response = client.get('/')
-    assert "200 OK" == response
+@patch('requests.get')
+def test_index_page(mock_get_requests, client):    
+    # Replace call to requests.get(url) with our own function    
+    mock_get_requests.side_effect = mock_get_cards
+    response = client.get('/') 
+    assert "200 OK" == response.status     
+    assert b"Test Todo" in response.data
+    assert b"Test Doing" in response.data
+    assert b"Test Done" in response.data    
+
+sample_trello_cards_response = [{
+    "id": "1",
+    "name": "Test Todo",
+    "idList": "5thnbgr43edcxsw2",
+    "dateLastActivity": "2020-10-30"
+},
+{
+    "id": "2",
+    "name": "Test Doing",
+    "idList": "8ikmnhy63tsgwyey",
+    "dateLastActivity": "2020-10-30"
+},
+{
+    "id": "3",
+    "name": "Test Done",
+    "idList": "a92je8dkw9dksowo",
+    "dateLastActivity": "2020-10-30"
+}
+]
+
+def mock_get_cards(url, params):    
+    if url == f"https://api.trello.com/1/boards/{os.getenv('board_id')}/cards":       
+        response = Mock()        
+        # sample_trello_cards_response should point to some test response data        
+        response.json.return_value = sample_trello_cards_response        
+        return response    
+    return None
+
--- trello_api.py
+++ trello_api.py
@@ -13,36 +13,36 @@ class TrelloApi:
         self.done_list_id = os.getenv('done_list_id')
 
     def get_board_cards(self):
-        endpoint = f"https://api.trello.com/1/boards/{self.board_id}/cards?"
+        endpoint = f"https://api.trello.com/1/boards/{self.board_id}/cards"
         response = requests.get(endpoint, params={"key": self.api_key, "token": self.token}).json()
         return response
 
     def get_board_lists(self):
-        endpoint = f"https://api.trello.com/1/boards/{self.board_id}/lists?"
+        endpoint = f"https://api.trello.com/1/boards/{self.board_id}/lists"
         response = requests.get(endpoint, params={"key": self.api_key, "token": self.token}).json()               
         return response
 
     def get_list_cards(self, card_id):
-        endpoint = f"https://api.trello.com/1/cards/{card_id}/list?"
+        endpoint = f"https://api.trello.com/1/cards/{card_id}/list"
         response = requests.get(endpoint, params={"key": self.api_key, "token": self.token}).json()
         return response
 
     def add_card(self, new_todo_item):
-        endpoint = f"https://api.trello.com/1/cards?"
+        endpoint = f"https://api.trello.com/1/cards"
         response = requests.post(endpoint, params={"name": new_todo_item, "key": self.api_key, "token": self.token, "idList": self.todo_list_id}).json()
         return response
 
     def move_card_not_started_in_progress(self, card_id):
-        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList?value={self.doing_list_id}"
-        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token}).json()
+        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList"
+        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token, "value": self.doing_list_id}).json()
         return response
 
     def move_card_in_progress_done(self, card_id):
-        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList?value={self.done_list_id}"
-        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token}).json()
+        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList"
+        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token, "value": self.done_list_id}).json()
         return response
 
     def move_card_done_todo(self, card_id):
-        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList?value={self.todo_list_id}"
-        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token}).json()
+        endpoint = f"https://api.trello.com/1/cards/{card_id}/idList"
+        response = requests.put(endpoint, params={"key": self.api_key, "token": self.token, "value": self.todo_list_id}).json()
         return response
